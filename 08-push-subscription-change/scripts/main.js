47a7d54e764a8a70772305b885cc3149437157de
Ivan Mladenov <ismladenov@gmail.com>
2018-10-05 09:36:15 +0300

    Update main.js

--- 08-push-subscription-change/scripts/main.js
+++ 08-push-subscription-change/scripts/main.js
@@ -21,7 +21,7 @@
 
 'use strict';
 
-const applicationServerPublicKey = 'BMq3o-yOZOL1A-KulhPWVq8E4buAAvmx28aG3BxJb_fuBlYQU257WvmIl3cI0v6R1SIWsmyX-mz5D70-fW-PoPY';
+const applicationServerPublicKey = 'BGV2Xvai77Nn0S-CSp4kCa5EaYDdp3bP7Dcq-qfhPSMWEjuSw_XxusXs6f25wf8fargBtLyMl3BpTL07I8-mwyo';
 
 const pushButton = document.querySelector('.js-push-btn');
 
